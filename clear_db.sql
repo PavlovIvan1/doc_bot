bb60011738558b21511e2db6f8b6bf1559b37bb4
PavlovIvan1 <pavlov-2011pavlov@yandex.ru>
2026-03-24 22:11:27 +0300

    fixed error

--- clear_db.sql
+++ clear_db.sql
@@ -1,25 +1,24 @@
 -- Очистка базы данных для тестирования
--- Выполните эти команды в MySQL клиенте (например, через phpMyAdmin или mysql CLI)
+-- Выполните эти команды в MySQL клиенте
 
 -- Удаление всех записей из дочерних таблиц (с FOREIGN KEY)
+DELETE FROM payment_request_documents;
+DELETE FROM payment_request_history;
+DELETE FROM data_change_requests;
+DELETE FROM work_reports;
 DELETE FROM documents;
-DELETE FROM uploaded_documents;
-DELETE FROM action_logs;
 DELETE FROM payment_requests;
+DELETE FROM admins;
 
--- Удаление всех пользователей
+-- Удаление всех пользователей (последней, т.к. на неё ссылаются)
 DELETE FROM users;
 
--- Сброс автоинкрементов (опционально, чтобы ID начинались с 1)
+-- Сброс автоинкрементов
 ALTER TABLE users AUTO_INCREMENT = 1;
 ALTER TABLE documents AUTO_INCREMENT = 1;
 ALTER TABLE payment_requests AUTO_INCREMENT = 1;
-ALTER TABLE uploaded_documents AUTO_INCREMENT = 1;
-ALTER TABLE action_logs AUTO_INCREMENT = 1;
-
--- Для полной очистки всех таблиц (раскомментируйте при необходимости):
--- TRUNCATE TABLE users;
--- TRUNCATE TABLE documents;
--- TRUNCATE TABLE payment_requests;
--- TRUNCATE TABLE uploaded_documents;
--- TRUNCATE TABLE action_logs;
+ALTER TABLE admins AUTO_INCREMENT = 1;
+ALTER TABLE data_change_requests AUTO_INCREMENT = 1;
+ALTER TABLE work_reports AUTO_INCREMENT = 1;
+ALTER TABLE payment_request_documents AUTO_INCREMENT = 1;
+ALTER TABLE payment_request_history AUTO_INCREMENT = 1;
